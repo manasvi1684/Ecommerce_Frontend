5b478920971aba9f58a822030406b73d16835fc0
Manasvi Gupta <143724123+manasvi1684@users.noreply.github.com>
2025-04-09 01:05:49 +0530

    Add files via upload

---
+++ src/pages/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+  const {productId} = userParams();
+  console.log(productId);
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
